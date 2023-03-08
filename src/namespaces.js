4ee08c43859f201db8a92bb1799b4b93a0e1a337
Cristian <cristianvasquez@gmail.com>
2023-03-08 01:35:20 +0100

    show a network of people

---
+++ src/namespaces.js
@@ -0,0 +1,12 @@
+import rdf from './rdf-ext.js'
+
+const ns = {
+  rdf: rdf.namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
+  schema: rdf.namespace('http://schema.org/'),
+  xsd: rdf.namespace('http://www.w3.org/2001/XMLSchema#'),
+  rdfs: rdf.namespace('http://www.w3.org/2000/01/rdf-schema#'),
+  ex: rdf.namespace('http://example.org/'),
+  dot: rdf.namespace('http://pkm-united.org/'),
+}
+
+export default ns
